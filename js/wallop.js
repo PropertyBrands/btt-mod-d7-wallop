645c580c92bc3aae4d48bef89e972fa6fc0e1633
Alex Nordhausen <anorthhouse@gmail.com>
2021-03-16 07:00:15 -0600

    feat: add ability to change autoplay enabled/disabled setting

--- js/wallop.js
+++ js/wallop.js
@@ -1,4 +1,63 @@
+
 (function($){
+  var nextTimeout;
+
+  var loadNext = function (current_slideshow, autoplay) {
+    var nextIndex = (current_slideshow.currentItemIndex + 1) % current_slideshow.allItemsArray.length;
+    current_slideshow.goTo(nextIndex);
+
+    // If this is an autoplay slideshow, then let's recursively load the next slide per the
+    // autoplay settings.
+    if(autoplay) {
+      nextTimeout = setTimeout(function () {
+        loadNext(current_slideshow, autoplay);
+      }, current_slideshow.options.autoPlayMs);
+    }
+  };
+
+  var autoplayWallopSlideshows = function(wallop_slideshows) {
+    wallop_slideshows.forEach(function(current_slideshow) {
+      // Get invidiual slide settings.
+      var autoPlayMs = current_slideshow.options.autoPlayMs;
+      var shouldAutoPlay = current_slideshow.options.shouldAutoPlay;
+
+      console.log(current_slideshow);
+      console.log(shouldAutoPlay);
+      if(current_slideshow && shouldAutoPlay) {
+        // Auto-run without requiring user change or mouseenter/mouseleave.
+        nextTimeout = setTimeout(function () {
+          loadNext(current_slideshow, shouldAutoPlay);
+        }, autoPlayMs);
+      }
+      else {
+        // If we don't need to set up autoplay for this slideshow, or it doesn't exist, then let's bail out.
+        return;
+      }
+      
+      var el = $(document.querySelector(current_slideshow.id));
+      // Wallop specific settings.
+      el.on('change', function () {
+        console.log("Changed sliedshow" . current_slideshow.id);
+        if(nextTimeout) {
+          clearTimeout(nextTimeout);
+        }
+        nextTimeout = setTimeout(function () {
+          loadNext(current_slideshow, shouldAutoPlay);
+        }, autoPlayMs);
+      });
+      el.on('mouseenter', function () {
+        if(nextTimeout) {
+          clearTimeout(nextTimeout);
+        }
+      });
+      el.on('mouseleave', function () {
+        nextTimeout = setTimeout(function () {
+          loadNext(current_slideshow, shouldAutoPlay);
+        }, autoPlayMs);
+      });
+    });
+  };
+
   Drupal.behaviors.wallop = {
     attach: function (context, settings) {
       var wallop_has_processed = document.querySelectorAll('.wallop-processed');
@@ -11,14 +70,12 @@
           var wallop_slideshows = [];
           for (var id in settings.wallop.instances) {
             // Slider element and settings from Drupal.
-            console.log(settings.wallop.instances);
-            console.log(id);
             var $slider_id = '#' + id,
               $slider_settings = settings.wallop.instances[id].settings,
               $slider,
-              $slideshow,
-              $shouldAutoPlay = $slider_settings.shouldAutoPlay || true,
-              $autoPlayMs = $slider_settings.autoPlayMs || 4500;
+              $slideshow;
+              // $shouldAutoPlay = $slider_settings.shouldAutoPlay,
+              // $autoPlayMs = $slider_settings.autoPlayMs || 4500;
 
             $slider = document.querySelector($slider_id);
 
@@ -37,49 +94,14 @@
             wallop_slideshows.push($slideshow);
           }
 
-          var loadNext = function (current_slideshow, autoplay = true) {
-            var nextIndex = (current_slideshow.currentItemIndex + 1) % current_slideshow.allItemsArray.length;
-            current_slideshow.goTo(nextIndex);
+          if(!wallop_slideshows.length) {
+            return;
+          }
 
-            if(autoplay) {
-              setTimeout(function () {
-                loadNext(current_slideshow, autoplay);
-              }, $autoPlayMs);
-            }
-          };
-
-          var nextTimeout;
-
-          wallop_slideshows.forEach(function(current_slideshow) {
-            // Auto-run without requiring user change or mouseenter/mouseleave.
-            setTimeout(function () {
-              loadNext(current_slideshow, $shouldAutoPlay);
-            }, $autoPlayMs);
-
-
-            var el = $(document.querySelector(current_slideshow.id));
-            // Wallop specific settings.
-            el.on('change', function () {
-              if(nextTimeout) {
-                clearTimeout(nextTimeout);
-              }
-              nextTimeout = setTimeout(function () {
-                loadNext(current_slideshow, $shouldAutoPlay);
-              }, $autoPlayMs);
-            });
-            el.on('mouseenter', function () {
-              if(nextTimeout) {
-                clearTimeout(nextTimeout);
-              }
-            });
-            el.on('mouseleave', function () {
-              nextTimeout = setTimeout(function () {
-                loadNext(current_slideshow);
-              }, $autoPlayMs);
-            });
-          });
+          autoplayWallopSlideshows(wallop_slideshows);
         }
       }
     },
   };
+
 })(jQuery);
